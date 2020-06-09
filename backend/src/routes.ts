21c17d968b08afb3d9f9aedaa3956c460f7d3f8a
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2020-06-09 13:00:06 +0100

    Commit de teste defuncionament

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express, { Router, Request, Response } from 'express'
 
 const routes = express.Router()
 
 //npm run dev
 
-routes.get('/', () => {
-    return('Hello World')
+routes.get('/', (Request, Response) => {
+    return Response.json('SPORTING CLUBE DE PORTUGAL!!')
 })
 
 export default routes;
